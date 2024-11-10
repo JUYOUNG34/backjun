d76a7ea126ef990a123037b4051b8608e44b7b0c
JUYOUNG34 <eunh9372@naver.com>
2024-11-10 20:48:16 +0900

    test_2675.java

--- test_2675.java
+++ test_2675.java
@@ -1,14 +1,22 @@
-
 import java.util.Scanner;
-public class Main {
+public class test_2675 {
  
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		Scanner a = new Scanner(System.in);
 		
-		int T = in.nextInt();
+		int T = a.nextInt();
 		for(int i = 0; i < T; i++) {
 	
-			int R = in.nextInt();
-			String S = in.next();	// nextLine() 을 쓰면 error!
+			int R = a.nextInt();
+			String S = a.next();	// nextLine() 을 쓰면 error!
 			
-		
+			for(int j = 0; j < S.length(); j++) {           
+				for(int k = 0; k < R; k++) {	// R 만큼 반복 출력
+					System.out.print(S.charAt(j));
+				}
+			}
+            
+			System.out.println();
+		}
+	}
+}
